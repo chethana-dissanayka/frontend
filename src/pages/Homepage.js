4951270f6c40f6045810ca451b71a11cafb2f152
chethana-dissanayaka <cchethana173@gmail.com>
2024-03-22 23:12:54 +0530

    FirstCommit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,21 @@
+import Footer from "../components/Footer";
+
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+        <Footer/>
+{/* 
+
+      <Footer
+        propAlignSelf="unset"
+        propPosition="absolute"
+        propTop="4450px"
+        propLeft="0px"
+        propWidth="100%"
+      /> */}
+    </div>
+  );
+};
+
+export default HomePage;
