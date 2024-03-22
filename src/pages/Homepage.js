d4369dacead729d48ccbb73e646de17b0c08b8b2
chethana-dissanayaka <cchethana173@gmail.com>
2024-03-22 23:47:47 +0530

    footer2

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,19 +1,12 @@
 import Footer from "../components/Footer";
+import Footer2 from "../components/Footer2";
 
 
 const HomePage = () => {
   return (
     <div className="home-page">
         <Footer/>
-{/* 
-
-      <Footer
-        propAlignSelf="unset"
-        propPosition="absolute"
-        propTop="4450px"
-        propLeft="0px"
-        propWidth="100%"
-      /> */}
+        {/* <Footer2/> */}
     </div>
   );
 };
